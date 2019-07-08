fd59cfb34a3f9f29418cb8af6fdc1654a478fd9c
Honza Javorek <mail@honzajavorek.cz>
2019-07-08 21:32:20 +0200

    release to homebrew

--- README.md
+++ README.md
@@ -11,3 +11,12 @@ When I browse photo files on my external drive connected to my MacBook and I use
 ```
 brew install https://raw.githubusercontent.com/honzajavorek/eject/master/eject.rb
 ```
+
+## Release
+
+1. `git tag v1.0.1`
+2. `git push origin master --tags`
+3. `wget https://github.com/honzajavorek/eject/archive/v1.0.1.zip`
+4. `sha256sum *.zip`
+5. Edit [eject.rb](eject.rb)
+6. `git commit -am "release to homebrew" && git push origin master`
--- eject.rb
+++ eject.rb
@@ -1,9 +1,9 @@
 class Eject < Formula
   desc "Force ejecting external drive on macOS"
   homepage "https://github.com/honzajavorek/eject"
-  url "https://github.com/honzajavorek/eject/archive/v1.0.0.zip"
-  version "1.0.0"
-  sha256 "39849bbb249bfd94491fffa4c140c4ae0e26f4b66565a9f0e506e879cfc08359"
+  url "https://github.com/honzajavorek/eject/archive/v1.0.1.zip"
+  version "1.0.1"
+  sha256 "f69dec5fffd3d59a55152132199c32a1df177f574fc59b62ee737c6febf845e8"
 
   depends_on "python" => "3"
 
